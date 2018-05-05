b165615a13375290f61225f6b2624327304ef9f2
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-05-05 12:21:30 +0100

    - shorten functions descriptions

--- zbx_mod_mysql.c
+++ zbx_mod_mysql.c
@@ -51,81 +51,69 @@ static int dummy_echo (AGENT_REQUEST * request, AGENT_RESULT * result)
 }
 
 /******************************************************************************
- *                                                                            *
- * Function: zbx_module_api_version                                           *
- *                                                                            *
- * Purpose: returns version number of the module interface                    *
- *                                                                            *
- * Return value: ZBX_MODULE_API_VERSION - version of module.h module is       *
- *               compiled with, in order to load module successfully Zabbix   *
- *               MUST be compiled with the same version of this header file   *
- *                                                                            *
- ******************************************************************************/
+ Function:	zbx_module_api_version
+
+ Purpose:	returns version number of the module interface
+ Return value:	ZBX_MODULE_API_VERSION - version of module.h module is
+                compiled with, in order to load module successfully Zabbix
+                MUST be compiled with the same version of this header file
+*******************************************************************************/
 int zbx_module_api_version (void)
 {
 	return ZBX_MODULE_API_VERSION;
 }
 
 /******************************************************************************
- *                                                                            *
- * Function: zbx_module_item_timeout                                          *
- *                                                                            *
- * Purpose: set timeout value for processing of items                         *
- *                                                                            *
- * Parameters: timeout - timeout in seconds, 0 - no timeout set               *
- *                                                                            *
- ******************************************************************************/
+ Function:	zbx_module_item_timeout
+
+ Purpose:	set timeout value for processing of items
+ Parameters:	timeout - timeout in seconds, 0 - no timeout set
+*******************************************************************************/
+
 void zbx_module_item_timeout (int timeout)
 {
 	item_timeout = timeout;
 }
 
 /******************************************************************************
- *                                                                            *
- * Function: zbx_module_item_list                                             *
- *                                                                            *
- * Purpose: returns list of item keys supported by the module                 *
- *                                                                            *
- * Return value: list of item keys                                            *
- *                                                                            *
- ******************************************************************************/
+ Function:	zbx_module_item_list
+
+ Purpose:	returns list of item keys supported by the module
+ Return value:	list of item keys
+*******************************************************************************/
+
 ZBX_METRIC *zbx_module_item_list (void)
 {
 	return keys;
 }
 
 /******************************************************************************
- *                                                                            *
- * Function: zbx_module_init                                                  *
- *                                                                            *
- * Purpose: the function is called on agent startup                           *
- *          It should be used to call any initialization routines             *
- *                                                                            *
- * Return value: ZBX_MODULE_OK - success                                      *
- *               ZBX_MODULE_FAIL - module initialization failed               *
- *                                                                            *
- * Comment: the module won't be loaded in case of ZBX_MODULE_FAIL             *
- *                                                                            *
- ******************************************************************************/
+ Function:	zbx_module_init
+
+ Purpose:	the function is called on agent startup
+		It should be used to call any initialization routines
+ Return value:	ZBX_MODULE_OK - success
+		ZBX_MODULE_FAIL - module initialization failed
+ Comment:	the module won't be loaded in case of ZBX_MODULE_FAIL
+*******************************************************************************/
+
 int zbx_module_init (void)
 {
 	/* initialization for dummy.random */
-	srand (time (NULL));
+	/* srand(time(NULL)); */
 
 	return ZBX_MODULE_OK;
 }
 
 /******************************************************************************
- *                                                                            *
- * Function: zbx_module_uninit                                                *
- *                                                                            *
- * Purpose: the function is called on agent shutdown                          *
- *          It should be used to cleanup used resources if there are any      *
- *                                                                            *
- * Return value: ZBX_MODULE_OK - success                                      *
- *               ZBX_MODULE_FAIL - function failed                            *
- *                                                                            *
- ******************************************************************************/
+ Function:	zbx_module_uninit
+
+ Purpose:	the function is called on agent shutdown
+		It should be used to cleanup used resources if there are any
+ Return value:	ZBX_MODULE_OK	- success
+		ZBX_MODULE_FAIL	- function failed
+*******************************************************************************/
+
 int zbx_module_uninit (void)
 {
 	return ZBX_MODULE_OK;
