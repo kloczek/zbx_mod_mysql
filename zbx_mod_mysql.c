bd00dbb387b72bfbbd19972ae4ed900bb2906f56
Tomasz Kłoczko <kloczek@fedoraproject.org>
2018-05-05 12:26:25 +0100

    - change dummy.echo[] key to mysql.db.discovery[] - change Copyright

--- zbx_mod_mysql.c
+++ zbx_mod_mysql.c
@@ -1,21 +1,20 @@
 /*
-** Zabbix
-** Copyright (C) 2001-2018 Zabbix SIA
-**
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-**/
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ Copyright 2018 Tomasz Kłoczko
+*/
 
 #include "sysinc.h"
 #include "module.h"
@@ -23,26 +22,19 @@
 /* the variable keeps timeout setting for item processing */
 static int item_timeout = 0;
 
-static int dummy_echo (AGENT_REQUEST * request, AGENT_RESULT * result);
+static int zbx_mod_mysql_db_discovery (AGENT_REQUEST * request,
+				       AGENT_RESULT * result);
 
 static ZBX_METRIC keys[] =
 /*	KEY			FLAG		FUNCTION	TEST PARAMETERS */
 {
-	{"dummy.echo", CF_HAVEPARAMS, dummy_echo, "a message"},
+	{"mysql.db.discovery", 0, zbx_mod_mysql_db_discovery, ""},
 	{NULL}
 };
 
-static int dummy_echo (AGENT_REQUEST * request, AGENT_RESULT * result)
+static int zbx_mod_mysql_db_discovery (AGENT_REQUEST * request,
+				       AGENT_RESULT * result)
 {
-	char *param;
-
-	if (1 != request->nparam) {
-		/* set optional error message */
-		SET_MSG_RESULT (result,
-				strdup ("Invalid number of parameters."));
-		return SYSINFO_RET_FAIL;
-	}
-
 	param = get_rparam (request, 0);
 
 	SET_STR_RESULT (result, strdup (param));
